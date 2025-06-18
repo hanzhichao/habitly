1cbe7416d632c3f557e20202bcb3fb8814eaf248
hzc <zhichao_han@sina.cn>
2025-06-18 13:17:33 +0800

    feat: 删除未使用的 sql

--- src/lib/habits.ts
+++ src/lib/habits.ts
@@ -1,21 +1,16 @@
 import Database from "@tauri-apps/plugin-sql";
 import {Habit, Record} from "@/lib/types";
 import * as path from "@tauri-apps/api/path";
-import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log';
+import { info } from '@tauri-apps/plugin-log';
 import {
   addDays,
-  differenceInDays,
-  endOfMonth,
   format,
-  isAfter,
-  isBefore,
-  isEqual,
-  startOfMonth,
   startOfWeek
 } from 'date-fns';
 import {exists} from "@tauri-apps/plugin-fs";
 
 
+// 创建表
 async function createTable() {
   // const dbFile = await path.join(await path.homeDir(), "Habitly/db.sqlite")
   await info("创建表")
@@ -25,13 +20,13 @@ async function createTable() {
             (
                 id          INTEGER PRIMARY KEY AUTOINCREMENT,
                 name        VARCHAR(64) NOT NULL,
-                description VARCHAR(64) DEFAULT "",
+                description VARCHAR(64) DEFAULT '',
                 goal        VARCHAR(255),
-                frequency   VARCHAR(64) DEFAULT "daily",
+                frequency   VARCHAR(64) DEFAULT 'daily',
                 reminder    VARCHAR(64),
-                icon        VARCHAR(20) DEFAULT "arrow-right",
-                color       VARCHAR(20) DEFAULT "bg-blue-500",
-                start_date  DATE        NOT NULL,
+                icon        VARCHAR(20) DEFAULT 'arrow-right',
+                color       VARCHAR(20) DEFAULT 'bg-blue-500',
+                start_date  DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
                 end_date    DATE,
                 create_at   DATETIME    DEFAULT CURRENT_TIMESTAMP,
                 update_at   DATETIME    DEFAULT CURRENT_TIMESTAMP
@@ -43,7 +38,7 @@ async function createTable() {
                 habit_id    INTEGER,
                 record_date DATE        DEFAULT CURRENT_TIMESTAMP,
                 status      TINYINT     DEFAULT 1,
-                note        VARCHAR(64) DEFAULT "",
+                note        VARCHAR(64) DEFAULT '',
                 create_at   DATETIME    DEFAULT CURRENT_TIMESTAMP,
                 update_at   DATETIME    DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (habit_id) REFERENCES habits (id)
@@ -55,23 +50,13 @@ async function createTable() {
 async function getDb() {
   // const dbFile = await path.join(await path.homeDir(), "Habitly/db.sqlite")
   const dbFile = await path.join(await path.appDataDir(), 'db.sqlite');
-  await info(`dbFile: ${dbFile}`)
+  // await info(`dbFile: ${dbFile}`)
   const dbFileExists = await exists(dbFile);
   if(!dbFileExists){
     await createTable()
   }
   return await Database.load("sqlite:" + dbFile);
 }
-
-// export async function getHabitsWithStatus(){
-//   const db = await getDb();
-//   const results = await db.select<Habit[]>(`SELECT id,name,goal,frequency,reminder,icon,color,COALESCE(records.status, 0) AS status
-//     FROM habits
-//     LEFT JOIN records ON habits.id = records.habit_id;`);
-//   console.log(results)
-//   return results
-// }
-
 
 export async function getHabitsWithStatusToday() {
   const db = await getDb();
@@ -100,16 +85,16 @@ export async function getHabitsWithStatus(day: string) {
            CASE WHEN records.status = 1 THEN 1 ELSE 0 END AS status
     FROM habits
            LEFT JOIN records ON habits.id = records.habit_id AND record_date = $1
-    WHERE date("now") >= date(start_date);`, [day])
+    WHERE date('now') >= date(start_date);`, [day])
   await info(habits.length.toString())
   return habits
 }
 
 
-export async function getTodayHabits() {
+export async function getTodayHabits()
+{
+  await info('getTodayHabits')
   const result = await getHabitsWithStatusToday()
-  console.log('getHabitsWithStatus')
-  console.log(result)
   const habits: Habit[] = [];
   const now = new Date();
   for (const item of result) {
@@ -130,17 +115,8 @@ export async function getTodayHabits() {
   return habits
 }
 
-export async function getCompleteCountOfDate(date: string) {
-  const db = await getDb();
-  const result = await db.select<{
-    count: number
-  }[]>("SELECT COUNT(record_id) as count FROM `records` WHERE record_date = $1;", [date]);
-  if (result.length === 0) return 0
-  return result[0].count
-}
-
-
 export async function getHabit(id: number) {
+  await info(`getHabit: id=${id}`)
   const db = await getDb();
   const result = await db.select<Habit[]>("SELECT * FROM `habits` WHERE id = $1;", [id]);
   return result.length > 0 ? result[0] : null
@@ -151,21 +127,14 @@ export async function addHabit(name: string, goal: string, frequency: string, re
   return await db.execute("INSERT INTO `habits` (name,goal,frequency,reminder,icon,color,start_date) VALUES ($1,$2,$3,$4,$5,$6,date('now'));", [name, goal, frequency, reminder, icon, color]);
 }
 
-export async function updateHabit(habbitId: number, name: string, goal: string, frequency: string, reminder: string, icon: string, color: string) {
+export async function updateHabit(habitId: number, name: string, goal: string, frequency: string, reminder: string, icon: string, color: string) {
   const db = await getDb();
-  return await db.execute("UPDATE `habits` SET name=$1,goal=$2,frequency=$3,reminder=$4,icon=$5,color=$6 WHERE id=$7;", [name, goal, frequency, reminder, icon, color, habbitId]);
+  return await db.execute("UPDATE `habits` SET name=$1,goal=$2,frequency=$3,reminder=$4,icon=$5,color=$6 WHERE id=$7;", [name, goal, frequency, reminder, icon, color, habitId]);
 }
 export async function deleteHabit(id: number) {
   const db = await getDb();
   await db.execute("DELETE FROM `records` WHERE habit_id=$1", [id]);
   return await db.execute("DELETE FROM `habits` WHERE id=$1;", [id]);
-}
-
-
-// 查询习惯所有记录列表
-export async function getRecords(habitId: number) {
-  const db = await getDb();
-  return db.select<Record[]>("SELECT * FROM `records` WHERE habit_id = $1 ORDER BY record_date;", [habitId]);
 }
 
 // 查询习惯完成记录列表
@@ -207,54 +176,6 @@ export async function toggleRecordStatusOfToday(habitId: number) {
     return await updateRecordOfToday(record.record_id, newStatus)
   }
   return addRecordOfToday(habitId)
-}
-
-
-// 统计习惯完成记录
-export async function getRecordStats(habitId: number) {
-  const records = await getCompletedRecords(habitId)
-  if (records.length === 0) return {completionRate: 0, currentStreak: 0, longestStreak: 0, monthlyCompletionRate: 0}
-
-  const now = new Date();
-  const recordStart = new Date(records[0].record_date)
-  const totalDays = Math.abs(differenceInDays(now, recordStart))
-  const completionRate = Math.round(records.length / totalDays * 100)
-  const monthStart = startOfMonth(now)
-  const monthEnd = endOfMonth(now)
-
-  let monthlyRecordCount = 0
-  if ((isEqual(recordStart, monthStart) || isAfter(recordStart, monthStart) &&
-      (isEqual(recordStart, monthEnd) || isBefore(recordStart, monthEnd)))){
-    monthlyRecordCount ++
-  }
-
-  let currentStreak = 1;
-  let longestStreak = 1;
-
-  let lastRecordDate = new Date(records[0].record_date)
-  for (const record of records.slice(1)) {
-    const recordDate = new Date(record.record_date)
-    if ((isEqual(recordDate, monthStart) || isAfter(recordDate, monthStart) &&
-      (isEqual(recordDate, monthEnd) || isBefore(recordDate, monthEnd)))){
-      monthlyRecordCount ++
-    }
-    const daysDiff = differenceInDays(recordDate, lastRecordDate);
-    if (daysDiff === 1) {
-      currentStreak++
-    } else {
-      currentStreak = 1
-    }
-    lastRecordDate = recordDate
-    longestStreak = Math.max(longestStreak, currentStreak);
-  }
-
-  const monthlyCompletionRate = Math.round(monthlyRecordCount / monthEnd.getDate() * 100)
-  console.log('completionRate', completionRate)
-  console.log('currentStreak', currentStreak)
-  console.log('longestStreak', longestStreak)
-  console.log('monthlyCompletionRate', monthlyCompletionRate)
-  return {completionRate: completionRate, currentStreak: currentStreak, longestStreak: longestStreak, monthlyCompletionRate: monthlyCompletionRate}
-
 }
 
 export async function getWeeklyData() {
