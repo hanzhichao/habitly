7ccf138e5c11bb01bbb9ac702ea712a65663cab0
hzc <zhichao_han@sina.cn>
2025-06-18 16:21:38 +0800

    feat: 调整 sql 语句，增加 localtime

--- src/lib/habits.ts
+++ src/lib/habits.ts
@@ -58,7 +58,7 @@ async function getDb() {
   return await Database.load("sqlite:" + dbFile);
 }
 
-export async function getHabitsWithStatusToday() {
+async function _getHabitsWithStatusToday() {
   const db = await getDb();
   return  await db.select<Habit[]>(`SELECT id,
                                                    name,
@@ -74,18 +74,16 @@ export async function getHabitsWithStatusToday() {
                                                    LEFT JOIN records ON habits.id = records.habit_id AND record_date = DATE('now');`);
 }
 
-export async function getHabitsWithStatus(day: string) {
+async function _getHabitsWithStatus(day: string) {
   await info(`getHabitsWithStatus: day=${day}`)
-
   const db = await getDb();
-  // const habits: Habit[] = []
   const habits = await db.select<Habit[]>(`
     SELECT id,name,goal,frequency,
            reminder,icon,color,start_date,end_date,
            CASE WHEN records.status = 1 THEN 1 ELSE 0 END AS status
     FROM habits
            LEFT JOIN records ON habits.id = records.habit_id AND record_date = $1
-    WHERE date('now') >= date(start_date);`, [day])
+    WHERE date('now', 'localtime') >= date(start_date, 'localtime');`, [day])
   await info(habits.length.toString())
   return habits
 }
@@ -94,7 +92,7 @@ export async function getHabitsWithStatus(day: string) {
 export async function getTodayHabits()
 {
   await info('getTodayHabits')
-  const result = await getHabitsWithStatusToday()
+  const result = await _getHabitsWithStatusToday()
   const habits: Habit[] = [];
   const now = new Date();
   for (const item of result) {
@@ -123,44 +121,48 @@ export async function getHabit(id: number) {
 }
 
 export async function addHabit(name: string, goal: string, frequency: string, reminder: string, icon: string, color: string) {
+  await info(`addHabit: name=${name}`)
   const db = await getDb();
-  return await db.execute("INSERT INTO `habits` (name,goal,frequency,reminder,icon,color,start_date) VALUES ($1,$2,$3,$4,$5,$6,date('now'));", [name, goal, frequency, reminder, icon, color]);
+  return await db.execute("INSERT INTO `habits` (name,goal,frequency,reminder,icon,color,start_date) VALUES ($1,$2,$3,$4,$5,$6,date('now', 'localtime'));", [name, goal, frequency, reminder, icon, color]);
 }
 
 export async function updateHabit(habitId: number, name: string, goal: string, frequency: string, reminder: string, icon: string, color: string) {
+  await info(`updateHabit: habitId=${habitId}`)
   const db = await getDb();
   return await db.execute("UPDATE `habits` SET name=$1,goal=$2,frequency=$3,reminder=$4,icon=$5,color=$6 WHERE id=$7;", [name, goal, frequency, reminder, icon, color, habitId]);
 }
-export async function deleteHabit(id: number) {
+export async function deleteHabit(habitId: number) {
+  await info(`deleteHabit: habitId=${habitId}`)
   const db = await getDb();
-  await db.execute("DELETE FROM `records` WHERE habit_id=$1", [id]);
-  return await db.execute("DELETE FROM `habits` WHERE id=$1;", [id]);
+  await db.execute("DELETE FROM `records` WHERE habit_id=$1", [habitId]);
+  return await db.execute("DELETE FROM `habits` WHERE id=$1;", [habitId]);
 }
 
 // 查询习惯完成记录列表
 export async function getCompletedRecords(habitId: number) {
+  await info(`getCompletedRecords: habitId=${habitId}`)
   const db = await getDb();
-  return db.select<Record[]>("SELECT * FROM `records` WHERE habit_id = $1 AND status = 1 ORDER BY record_date;", [habitId]);
+  return db.select<Record[]>("SELECT * FROM `records` WHERE habit_id = $1 AND status = 1 ORDER BY date(record_date, 'localtime');", [habitId]);
 }
 
 
 // 查询习惯今日完成记录
-export async function getRecordOfToday(habitId: number) {
+async function _getRecordOfToday(habitId: number) {
   const db = await getDb();
-  const records = await db.select<Record[]>("SELECT * FROM records WHERE habit_id=$1 AND records.record_date=date('now');", [habitId])
+  const records = await db.select<Record[]>("SELECT * FROM records WHERE habit_id=$1 AND records.record_date=date('now', 'localtime');", [habitId])
   if (records.length === 0) return
   return records[0]
 }
 
 // 添加习惯今日完成记录
-export async function addRecordOfToday(habitId: number) {
+async function _addRecordOfToday(habitId: number) {
   const db = await getDb();
-  return db.execute("INSERT INTO records (habit_id,record_date) VALUES ($1,date('now'))", [habitId]);
+  return db.execute("INSERT INTO records (habit_id,record_date) VALUES ($1,date('now', 'localtime'))", [habitId]);
 }
 
 
 // 更新习惯今日完成记录
-export async function updateRecordOfToday(recordId: number, status: number) {
+async function _updateRecordOfToday(recordId: number, status: number) {
   const db = await getDb();
   return db.execute(`UPDATE records
                      SET status=$1
@@ -170,12 +172,13 @@ export async function updateRecordOfToday(recordId: number, status: number) {
 
 // 切换习惯今日完成记录完成状态
 export async function toggleRecordStatusOfToday(habitId: number) {
-  const record = await getRecordOfToday(habitId)
+  await info(`toggleRecordStatusOfToday: habitId=${habitId}`)
+  const record = await _getRecordOfToday(habitId)
   if (typeof record !== "undefined") {
     const newStatus = record.status === 1 ? 0 : 1
-    return await updateRecordOfToday(record.record_id, newStatus)
+    return await _updateRecordOfToday(record.record_id, newStatus)
   }
-  return addRecordOfToday(habitId)
+  return _addRecordOfToday(habitId)
 }
 
 export async function getWeeklyData() {
@@ -189,7 +192,7 @@ export async function getWeeklyData() {
     if (i < now.getDay()) {
       const date = addDays(startOfWeekDate, i);
       const dateStr = format(date, 'yyyy-MM-dd')
-      const habits = await getHabitsWithStatus(dateStr)
+      const habits = await _getHabitsWithStatus(dateStr)
       if (typeof habits === "undefined") continue
       const completeHabits = habits.filter(item => item.status == 1)
       const progress = habits.length !== 0 ? Math.round(completeHabits.length / habits.length * 100) : 0
